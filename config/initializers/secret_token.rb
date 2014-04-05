1eea2ffee2d08cab86a4cdef323ee75cb1712f19
Naomi <naomialysewilcox@gmail.com>
2014-04-04 23:33:50 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bitcamp::Application.config.secret_key_base = 'fad824b9bd14add5605cdcfc33598998aff59c2c2c70694835959a457616776781e2ee540a6065d99d98b1442ab0e868144c7672c46f16db8c0861e89ed694e1'
